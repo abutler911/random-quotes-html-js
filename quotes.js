8792701aaa93860247599cd45f453535e8cd74bf
abutler911 <abutler911@gmail.com>
2023-03-04 08:13:40 -0700

    styling changes

--- quotes.js
+++ quotes.js
@@ -1,10 +1,15 @@
 const quoteContainer = document.querySelector("#quote");
+const author = document.querySelector("#author");
 const quoteButton = document.querySelector("#quote-btn");
 let url = "https://type.fit/api/quotes";
 
 quoteButton.addEventListener("click", () => {
   getQuote().then((quote) => {
-    quoteContainer.innerHTML = quote;
+    quoteContainer.innerHTML = quote.text;
+    if (!author) {
+      author.innerHTML = `-- Anonymous`;
+    }
+    author.innerHTML = `-- ${quote.author}`;
   });
 });
 
@@ -13,5 +18,5 @@ const getQuote = async function () {
   const response = await fetch(url);
   const data = await response.json();
 
-  return data[randomNumber].text;
+  return data[randomNumber];
 };
